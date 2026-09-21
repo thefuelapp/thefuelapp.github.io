40d1d79c29c4414ae758c8b15b85f0e871880471
thefuelapp <thefuelapp@users.noreply.github.com>
2026-09-21 14:02:51 +0100

    deploy 2026-09-21

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import { CONFIG } from './config.js';
 import { cloud, initCloud, onCloudChange, signIn, signUp, resetPassword, redeemCode, signOut, hasAccess, pullState, pushStateSoon } from './cloud.js';
 
 const KEY = 'fuel:v1';
-const APP_VERSION = 'v77';
+const APP_VERSION = 'v78';
 const DATA = { ingredients: [], recipes: [] };
 const S = load();
 if (S.tab === 'settings') S.tab = S.prevTab && S.prevTab !== 'settings' ? S.prevTab : 'plan';
